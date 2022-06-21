4ff0cb960ffef96b2987282709f11e53e48bb976
kemaltt <ttnckemal@gmail.com>
2022-06-21 10:47:53 +0200

    categoryList

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -32,21 +32,22 @@ const CategoryList = () => {
   return (
     <section className="categoryListContainer">
       <h1>Or go through our categories</h1>
+      <div className="categoryList">
+        {data.map((el, i) => (
+          <CategoryItem
+            key={i}
+            id={el.idCategory}
+            name={el.strCategory}
+            img={el.strCategoryThumb}
+          />
+        ))}
 
-      {data.map((el, i) => (
-        <CategoryItem
-          key={i}
-          id={el.idCategory}
-          name={el.strCategory}
-          img={el.strCategoryThumb}
-        />
-      ))}
-
-      <Link to={`/detail/${randomId}`}>
-        <div className="randomContainer">
-          <h3>Random</h3>
-        </div>
-      </Link>
+        <Link to={`/detail/${randomId}`}>
+          <div className="randomContainer">
+            <h3>Random</h3>
+          </div>
+        </Link>
+      </div>
     </section>
   );
 };
