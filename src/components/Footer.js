470ab6dfae406b8e0cd98241ea941b8a9fd84eab
EliasFredoni <fredoni.elias@gmail.com>
2022-06-22 11:04:13 +0200

    link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
   return (
     <section className="footerContainer">
-      <i class="lab la-instagram"></i>
-      <i class="lab la-youtube"></i>
-      <i class="lab la-facebook-f"></i>
+      <i class="lab la-instagram"><a href="https://www.instagram.com/"></a></i>
+      <i class="lab la-youtube"><a href="https://www.youtube.com/"></a></i>
+      <i class="lab la-facebook-f"><a href="https://de-de.facebook.com/"></a></i>
     </section>
   );
 };
--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 
 const MealItem = ({ img, id, name, i }) => {
-  return (
-    <Link to={`/detail/${id}`}>
-      <section
-        className="mealItemContainer"
-        style={{ backgroundColor: i % 2 === 0 ? "  #FF6E85" : "#ffab61e8" }}
-      >
-        <h1>{name}</h1>
-        <img src={img} alt={img} />
-      </section>
-    </Link>
-  );
+    return (
+        <Link to={`/detail/${id}`}>
+            <section
+                className="mealItemContainer"
+                style={{ backgroundColor: i % 2 === 0 ? "  #FF6E85" : "#ffab61e8" }}
+            >
+                <h1>{name}</h1>
+                <img src={img} alt={img} />
+            </section>
+        </Link>
+    );
 };
 
 export default MealItem;
