2f0877d993c6e64fb69b81ee2c3342119e2e4b9e
EliasFredoni <fredoni.elias@gmail.com>
2022-06-24 11:44:00 +0200

    fehler

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -6,33 +6,6 @@ import AuthContext from "../context/AuthContext";
 const CategoryList = () => {
   const { data, randomId } = useContext(AuthContext);
 
-<<<<<<< HEAD
-  //   const [data, setData] = useState([]);
-  //   const [random, setRandom] = useState([]);
-
-  //   let test;
-
-  //   useEffect(() => {
-  //     fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-  //       .then((res) => res.json())
-  //       .then((json) => setData(json.categories));
-  //   }, [test]);
-
-  //   console.log(data);
-
-  //   useEffect(() => {
-  //     fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-  //       .then((res) => res.json())
-  //       .then((json) => setRandom(json.meals[0]));
-  //   }, [test]);
-
-  //   //   console.log(random);
-
-  //   const randomId = random.idMeal;
-=======
-  console.log(randomId);
->>>>>>> 590b23d10d5f0e0c78ec06b17f0589d95c32387c
-
   return (
     <section className="categoryListContainer">
       <h1>Or go through our categories <a href="arrow1"><span>↓</span></a></h1>
